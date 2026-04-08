a39c271caf37d946137dedd05cd661891dd442d0
Kuestamg <100108220+Kuestamg@users.noreply.github.com>
2026-04-08 17:05:19 -0600

    Add files via upload

---
+++ control_escolar/conex.php
@@ -0,0 +1,3 @@
+<?php 
+	$conn = mysqli_connect("mi-contenedor-mysql:3306","root","12345","midb"); 
+?> 
---
+++ control_escolar/index.php
@@ -0,0 +1,17 @@
+<?php 
+    include("header.php");
+
+echo "Tipo acceso Administrador";
+        $hoy = date("Y-m-d");
+
+        echo '<section class="content">
+                <div class="container-fluid">
+                    <div class="block-header">
+                          <div class="block-header">
+                        <h2>Bienvenid@: </h2>
+                    </div>
+                </div>
+            </section>';
+
+    include("footer.php");
+?>
